c363de889a6da770abab9eff9e542624ead5e7b0
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-10-15 10:34:03 +0200

    setup tests

--- src/app/components/roman-numeral-converter/roman-numeral-converter.component.spec.ts
+++ src/app/components/roman-numeral-converter/roman-numeral-converter.component.spec.ts
@@ -1,22 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  TestBedStatic,
+} from '@angular/core/testing';
 
 import { RomanNumeralConverterComponent } from './roman-numeral-converter.component';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { NumeralRepoService } from '../../services/numeral-repo.service';
+import { Observable, of } from 'rxjs';
 
 describe('RomanNumeralConverterComponent', () => {
   let component: RomanNumeralConverterComponent;
   let fixture: ComponentFixture<RomanNumeralConverterComponent>;
+  let numeralRepo: NumeralRepoService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RomanNumeralConverterComponent ]
-    })
-    .compileComponents();
+      declarations: [RomanNumeralConverterComponent],
+      providers: [FormBuilder, HttpClient, HttpHandler],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RomanNumeralConverterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    numeralRepo = TestBed.inject(NumeralRepoService);
+    spyOn(numeralRepo, 'convertRomanToArab').and.returnValue(
+      new Observable((obs) => {
+        obs.next({
+          nombreEnChiffreArabe: '10',
+          nombreEnChiffreRomain: 'X',
+        });
+        obs.complete();
+      })
+    );
   });
 
   it('should create', () => {
--- src/app/services/numeral-repo.service.spec.ts
+++ src/app/services/numeral-repo.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NumeralRepoService } from './numeral-repo.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('NumeralRepoService', () => {
   let service: NumeralRepoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler],
+    });
     service = TestBed.inject(NumeralRepoService);
   });
 
